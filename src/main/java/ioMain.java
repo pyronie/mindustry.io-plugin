efd6ebb47fd5cd65f35f4b9755542194c03797e6
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-03-21 11:49:57 +0100

    Revert "remove useless dependencies"
    This reverts commit 89f050f604dd82de673f4d1c057b35929d7bc085.

--- src/main/java/ioMain.java
+++ src/main/java/ioMain.java
@@ -7,35 +7,30 @@ import org.json.JSONObject;
 
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.MediaType;
-import java.util.concurrent.CompletableFuture;
 
-public class iobans extends Plugin {
+public class ioMain extends Plugin {
     public boolean enabled = true;
     public String kickReason = "You are banned from the io server network.\nYou can't join.";
-
+    
     //register event handlers and create variables in the constructor
-    public iobans() {
+    public ioMain() {
         enabled = Core.settings.getBool("io-global-bans-enabled", true);
 
         Events.on(EventType.PlayerConnect.class, event ->{
-            if(enabled) {
-                if (!event.player.isAdmin) {
-                    CompletableFuture.runAsync(() -> { // run async to prevent ping spikes
-                        String uuid = event.player.uuid;
-                        String url = "http://mindustry.io:8080/stats?uuid=" + uuid;
-                        try {
-                            JSONObject json = new JSONObject(ClientBuilder.newClient().target(url).request().accept(MediaType.APPLICATION_JSON).get(String.class));
-                            if (json.has("error") && !json.getBoolean("error")) {
-                                JSONObject data = json.getJSONObject("data");
-                                if (data.has("banned") && data.getBoolean("banned")) {
-                                    event.player.con.kick(kickReason);
-                                    Log.info("<.io>: Kicking " + event.player.name + ".");
-                                }
-                            }
-                        } catch (Exception e) {
-                            Log.info("<.io>: Connection with mindustry.io authentication server failed.");
+            if(!event.player.isAdmin) {
+                String uuid = event.player.uuid;
+                String url = "http://mindustry.io:8080/stats?uuid=" + uuid;
+                try {
+                    JSONObject json = new JSONObject(ClientBuilder.newClient().target(url).request().accept(MediaType.APPLICATION_JSON).get(String.class));
+                    if(json.has("error") && !json.getBoolean("error")) {
+                        JSONObject data = json.getJSONObject("data");
+                        if(data.has("banned") && data.getBoolean("banned")){
+                            event.player.con.kick(kickReason);
+                            Log.info("<.io>: Kicking " + event.player.name + ".");
                         }
-                    });
+                    }
+                } catch (Exception e) {
+                    Log.info("<.io>: Connection with mindustry.io authentication server failed.");
                 }
             }
         });
