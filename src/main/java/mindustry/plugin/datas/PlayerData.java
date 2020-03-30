47d90c25ce7f641059701f2f9f46dd819f402cd9
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-03-30 21:36:32 +0200

    add achievements to PlayerData

--- src/main/java/mindustry/plugin/datas/PlayerData.java
+++ src/main/java/mindustry/plugin/datas/PlayerData.java
@@ -1,17 +1,26 @@
 package mindustry.plugin.datas;
 
+import mindustry.entities.type.Player;
+
+import static mindustry.plugin.datas.Achievements.*;
+
 public class PlayerData implements Cloneable{
-    public int highiestWave;
-    public int achievements;
+    public int highestWave;
+    public Achievements achievements;
     public int role;
 
     public long bannedUntil = 0;
     public String banReason = "";
 
-    public PlayerData() {}
+    public PlayerData(Player player) {
+        achievements = new Achievements(player.uuid);
+    }
 
-    public void reprocess(){
-        if(banReason == null) this.banReason = "";
+    public void reprocess(Player player){
+        if(banReason == null || achievements == null){
+            this.banReason = "";
+            this.achievements = new Achievements(player.uuid);
+        }
     }
 
     public Object clone()throws CloneNotSupportedException{return super.clone();}
