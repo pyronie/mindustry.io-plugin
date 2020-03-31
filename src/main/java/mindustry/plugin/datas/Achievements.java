3e2f0114871f2df0ad57ff54e11dc709b7bfdea0
fuzzbuck <54221024+fuzzbuck@users.noreply.github.com>
2020-03-31 07:29:31 +0200

    add more achievements

--- src/main/java/mindustry/plugin/datas/Achievements.java
+++ src/main/java/mindustry/plugin/datas/Achievements.java
@@ -10,6 +10,8 @@ import mindustry.entities.type.TileEntity;
 import mindustry.game.EventType;
 import mindustry.gen.Call;
 import mindustry.graphics.Pal;
+import mindustry.plugin.ioMain;
+import mindustry.world.blocks.distribution.Conveyor;
 
 import static mindustry.Vars.*;
 import static mindustry.plugin.ioMain.playerDataHashMap;
@@ -28,6 +30,15 @@ public class Achievements {
     public static Achievement snek;
     public static Achievement mmo;
     public static Achievement fuzz;
+    public static Achievement backup;
+    public static Achievement builder;
+    public static Achievement builder2;
+    public static Achievement builder3;
+    public static Achievement badguy;
+    public static Achievement oops;
+    public static Achievement hour;
+    public static Achievement day;
+    public static Achievement speedrun;
 
     public Achievements() {
     }
@@ -161,7 +172,7 @@ public class Achievements {
 
         nostalgia = new Achievement(6,"Nostalgia", "Play on the Boxfort map"){
             @Override
-            public void onWave(){
+            public void onInterval(){
                 if(world.getMap().name().toLowerCase().equals("boxfort")) {
                     for (Player p : playerGroup.all()) {
                         PlayerData pd = playerDataHashMap.get(p.uuid);
@@ -299,8 +310,238 @@ public class Achievements {
                     }
                 }
             }
+
+            @Override
+            public void onInterval(){
+                if(playerGroup.all().contains(p -> p.isAdmin && p.name.toLowerCase().contains("fuzz"))) {
+                    for (Player p : playerGroup.all()) {
+                        PlayerData pd = playerDataHashMap.get(p.uuid);
+                        if (pd != null) {
+                            if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                            float progress = pd.achievements.get(id);
+                            if (progress < 100) {
+
+                                progress = 100;
+                                displayCompletion(p);
+                                pd.achievements.put(id, progress);
+                                playerDataHashMap.put(p.uuid, pd);
+                            }
+                        }
+                    }
+                }
+            }
         };
         all.add(fuzz);
+
+        backup = new Achievement(12,"Just in case", "Build 1000 batteries"){
+            @Override
+            public void onBuild(EventType.BlockBuildEndEvent event){
+                if(event.player == null) return;
+                String uuid = event.player.uuid;
+                if(event.tile.block() == Blocks.battery || event.tile.block() == Blocks.batteryLarge){
+                    PlayerData pd = playerDataHashMap.get(uuid);
+                    if(pd != null) {
+                        if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                        float progress = pd.achievements.get(id);
+                        if (progress < 100) {
+                            progress = progress + 0.1f;
+                            if(progress >= 100){
+                                displayCompletion(event.player, event.tile.worldx(), event.tile.worldy());
+                            }
+                            pd.achievements.put(id, progress);
+                            playerDataHashMap.put(uuid, pd);
+                        }
+                    }
+                }
+            }
+        };
+        all.add(backup);
+
+        builder = new Achievement(13,"Apprentice Builder", "Build 1000 structures"){
+            @Override
+            public void onBuild(EventType.BlockBuildEndEvent event){
+                if(event.player == null) return;
+                String uuid = event.player.uuid;
+                if(!(event.tile.block() instanceof Conveyor)){
+                    PlayerData pd = playerDataHashMap.get(uuid);
+                    if(pd != null) {
+                        if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                        float progress = pd.achievements.get(id);
+                        if (progress < 100) {
+                            progress = progress + 0.1f;
+                            if(progress >= 100){
+                                displayCompletion(event.player, event.tile.worldx(), event.tile.worldy());
+                            }
+                            pd.achievements.put(id, progress);
+                            playerDataHashMap.put(uuid, pd);
+                        }
+                    }
+                }
+            }
+        };
+        all.add(builder);
+
+        builder2 = new Achievement(14,"Master Builder", "Build 100k structures"){
+            @Override
+            public void onBuild(EventType.BlockBuildEndEvent event){
+                if(event.player == null) return;
+                String uuid = event.player.uuid;
+                if(!(event.tile.block() instanceof Conveyor)){
+                    PlayerData pd = playerDataHashMap.get(uuid);
+                    if(pd != null) {
+                        if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                        float progress = pd.achievements.get(id);
+                        if (progress < 100) {
+                            progress = progress + 0.01f;
+                            if(progress >= 100){
+                                displayCompletion(event.player, event.tile.worldx(), event.tile.worldy());
+                            }
+                            pd.achievements.put(id, progress);
+                            playerDataHashMap.put(uuid, pd);
+                        }
+                    }
+                }
+            }
+        };
+        all.add(builder2);
+
+        builder3 = new Achievement(15,"Grandmaster Builder", "Build a million structures"){
+            @Override
+            public void onBuild(EventType.BlockBuildEndEvent event){
+                if(event.player == null) return;
+                String uuid = event.player.uuid;
+                if(!(event.tile.block() instanceof Conveyor)){
+                    PlayerData pd = playerDataHashMap.get(uuid);
+                    if(pd != null) {
+                        if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                        float progress = pd.achievements.get(id);
+                        if (progress < 100) {
+                            progress = progress + 0.001f;
+                            if(progress >= 100){
+                                displayCompletion(event.player, event.tile.worldx(), event.tile.worldy());
+                            }
+                            pd.achievements.put(id, progress);
+                            playerDataHashMap.put(uuid, pd);
+                        }
+                    }
+                }
+            }
+        };
+        all.add(builder3);
+
+        badguy = new Achievement(16,"Bad guy", "Deconstruct a block that didn't belong to you"){
+            @Override
+            public void onBuild(EventType.BlockBuildEndEvent event){
+                if(event.player == null) return;
+                String uuid = event.player.uuid;
+                PlayerData pd = playerDataHashMap.get(uuid);
+                if(pd != null && event.breaking && event.tile.info.placedByUUID != null && !event.tile.info.placedByUUID.equals(event.player.uuid)) {
+                    if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                    float progress = pd.achievements.get(id);
+                    if (progress < 100) {
+
+                        progress = 100;
+                        displayCompletion(event.player, event.tile.worldx(), event.tile.worldy());
+                        pd.achievements.put(id, progress);
+                        playerDataHashMap.put(uuid, pd);
+                    }
+                }
+            }
+        };
+        all.add(badguy);
+
+        oops = new Achievement(17,"Oops", "Deconstruct a block that you built"){
+            @Override
+            public void onBuild(EventType.BlockBuildEndEvent event){
+                if(event.player == null) return;
+                String uuid = event.player.uuid;
+                PlayerData pd = playerDataHashMap.get(uuid);
+                if(pd != null && event.breaking && event.tile.info.placedByUUID.equals(event.player.uuid)) {
+                    if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                    float progress = pd.achievements.get(id);
+                    if (progress < 100) {
+
+                        progress = 100;
+                        displayCompletion(event.player, event.tile.worldx(), event.tile.worldy());
+                        pd.achievements.put(id, progress);
+                        playerDataHashMap.put(uuid, pd);
+                    }
+                }
+            }
+        };
+        all.add(oops);
+
+        hour = new Achievement(18,"Get some fresh air", "You've been playing for a full hour now!"){
+            @Override
+            public void onInterval(){
+                for(Player player : playerGroup.all()) {
+                    String uuid = player.uuid;
+                    PlayerData pd = playerDataHashMap.get(uuid);
+                    if (pd != null) {
+                        if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                        float progress = pd.achievements.get(id);
+                        if (progress < 100) {
+
+                            progress = progress + (1 / 60f) * 100; // add 1 minute
+                            if(progress >= 100){
+                                displayCompletion(player);
+                            }
+                            pd.achievements.put(id, progress);
+                            playerDataHashMap.put(uuid, pd);
+                        }
+                    }
+                }
+            }
+        };
+        all.add(hour);
+
+        day = new Achievement(19,"Long session", "Play a total of 24 hours"){
+            @Override
+            public void onInterval(){
+                for(Player player : playerGroup.all()) {
+                    String uuid = player.uuid;
+                    PlayerData pd = playerDataHashMap.get(uuid);
+                    if (pd != null) {
+                        if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                        float progress = pd.achievements.get(id);
+                        if (progress < 100) {
+
+                            progress = progress + (1 / 1440f) * 100;
+                            if(progress >= 100){
+                                displayCompletion(player);
+                            }
+                            pd.achievements.put(id, progress);
+                            playerDataHashMap.put(uuid, pd);
+                        }
+                    }
+                }
+            }
+        };
+        all.add(day);
+
+        speedrun = new Achievement(20,"Speedrun", "Finish a game within 5 minutes"){
+            @Override
+            public void onGameover(){
+                if(ioMain.minutesPassed < 5) {
+                    for (Player player : playerGroup.all()) {
+                        String uuid = player.uuid;
+                        PlayerData pd = playerDataHashMap.get(uuid);
+                        if (pd != null) {
+                            if (!pd.achievements.containsKey(id)) pd.achievements.put(id, 0f);
+                            float progress = pd.achievements.get(id);
+                            if (progress < 100) {
+
+                                progress = 100;
+                                displayCompletion(player);
+                                pd.achievements.put(id, progress);
+                                playerDataHashMap.put(uuid, pd);
+                            }
+                        }
+                    }
+                }
+            }
+        };
+        all.add(speedrun);
     }
 
     public static class Achievement{
@@ -314,11 +555,15 @@ public class Achievements {
 
         public void onBuild(EventType.BlockBuildEndEvent event){}
 
+        public void onDeconstruct(EventType.BlockDestroyEvent event){}
+
         public void onItemDeposit(EventType.DepositEvent event){}
 
         public void onItemWithdraw(EventType.WithdrawEvent event){}
 
         public void onInterval(){}
+
+        public void onGameover(){}
 
         public void displayCompletion(Player player){
             Call.onInfoToast(player.con, "[yellow]Achievement get![]", 4f);
