9f660fc80484add0f587b33f1fc7dc9015b6f6ff
stubbEnable <69414142+stubbEnable@users.noreply.github.com>
2020-12-03 22:36:04 +0300

    redundant ` removal

--- src/main/java/mindustry/plugin/utils/Funcs.java
+++ src/main/java/mindustry/plugin/utils/Funcs.java
@@ -117,7 +117,7 @@ public class Funcs {
     }
 
     public static String escapeColorCodes(String string){
-        return Strings.stripColors(string).replaceAll("`", "");
+        return Strings.stripColors(string);
     }
 
     public static mindustry.maps.Map getMapBySelector(String query) {
